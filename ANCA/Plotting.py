9853dc15d154eb00ff58540d9297c3abd69f1de5
Archit Vasan <avasan@lambda0.cels.anl.gov>
2024-05-31 16:45:58 -0700

    added Plotting for ANCA

--- ANCA/Plotting.py
+++ ANCA/Plotting.py
@@ -1,8 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
 import matplotlib.pyplot as plt
-
+from pathlib import Path
 
 def show_scatter(data: np.ndarray, color: np.ndarray, output: str):
     fig = plt.figure()
@@ -13,46 +12,58 @@ def show_scatter(data: np.ndarray, color: np.ndarray, output: str):
     plt.savefig(output)
     plt.close()
 
-dihedrals = np.load('run-4/selection_runs/selection-0/dihedrals.npy')
-print(dihedrals.shape)
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-d',
+                    '--dihdata',
+                    type=str,
+                    help='Dihedral Data')
+
+parser.add_argument('-S',
+                    '--Sdata',
+                    type=Path,
+                    help='S Data')
+
+parser.add_argument('-e',
+                    '--ermsd',
+                    type=Path,
+                    help='e-rmsd data',
+                    )
+
+parser.add_argument('-Z',
+                    '--ZPrj4',
+                    type=Path,
+                    help='ZProj sd4 data',
+                    )
+
+parser.add_argument('-o',
+                    '--output',
+                    type=Path,
+                    help='Output path for Images',
+                    )
+
+args = parser.parse_args()
+
+
+dihedrals = np.load(args.dihdata)
 
 for i in range(dihedrals.shape[1]):
     dihedral_test = dihedrals[:,i,0]
-    print(dihedral_test)
-
-if False:
-    print("\n")
-    print(dihedrals.shape[1])
-    for i in range(dihedrals.shape[1]):
-        dihedral_test = dihedrals[:][1][i]
-        print(dihedral_test)
-    
-    print("\n")
-    for i in range(dihedrals.shape[2]):
-        dihedral_test = dihedrals[:][2][i]
-        print(dihedral_test)
-    
-    print("\n")
-    for i in range(dihedrals.shape[2]):
-        dihedral_test = dihedrals[:][3][i]
-        print(dihedral_test)
-
 
 plt.plot(dihedral_test[::100])
-plt.savefig('cos_dihedral_test.png')
+plt.savefig(f'{args.output}/cos_dihedral_test.png')
 plt.close()
 
-S = np.load('run-4/selection_runs/selection-0/S.npy')
+S = np.load(args.Sdata)
 plt.semilogy(S, "ro-")
-plt.savefig('S.png')
+plt.savefig(f'{args.output}/S.png')
 plt.close()
 
-e_rmsd = np.load('run-4/e_rmsd.npy')
-ZPrj4 = np.load('run-4/selection_runs/selection-0/sd4_projection.npy')
-print(ZPrj4.shape)
-print(len(e_rmsd))
+e_rmsd = np.load(args.ermsd)
+ZPrj4 = np.load(args.ZPrj4)
 
-show_scatter(ZPrj4[:, :3], e_rmsd[-1], 'ZPrj4.png')
+show_scatter(ZPrj4[:, :3], e_rmsd[-1], f'{args.output}/ZPrj4.png')
 
-z = np.load('run-4/selection_runs/selection-0/z.npy')
-show_scatter(z, e_rmsd[-1], 'z_ermsds.png')
+#z = np.load('run-4/selection_runs/selection-0/z.npy')
+#show_scatter(z, e_rmsd[-1], 'z_ermsds.png')
